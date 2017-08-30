df37e7676e4e9a2d63a6a6501ae99329009923c4
piyanattung <31474579+piyanattung@users.noreply.github.com>
2017-08-30 19:29:50 +0700

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -5,12 +5,17 @@ class Hello {
     public Hello() {
         System.out.println("Hello! We are:");
         this.Krerk();
+		this.Piyanat() ;
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
         System.out.println("  Demo  - Krerk Piromsopa, Ph.D.");
     }
+	
+	private void Piyanat(){
+        System.out.println("  Demo  - Piyanat Kongkaew");
+	}
     
     public static void main(String [] argv) {
         new Hello();
